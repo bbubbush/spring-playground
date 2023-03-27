dd1bdc472cfd9149e5fd67ce13b68652a78a17e4
bbubbush <bbubbush@gmail.com>
2023-03-27 23:52:49 +0900

    Feat: AppRunner로 기초 데이터 세팅 및 memberService에 save() 추가

---
+++ src/main/java/com/bbubbush/springplayground/runner/MemberAppRunner.java
@@ -0,0 +1,22 @@
+package com.bbubbush.springplayground.runner;
+
+import com.bbubbush.springplayground.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.IntStream;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MemberAppRunner implements CommandLineRunner {
+  private final MemberService memberService;
+  @Override
+  public void run(String... args) throws Exception {
+    log.debug("Hello111");
+    IntStream.range(0, 5)
+      .forEach(i -> memberService.save("bbubbush" + i));
+  }
+}
--- src/main/java/com/bbubbush/springplayground/service/MemberService.java
+++ src/main/java/com/bbubbush/springplayground/service/MemberService.java
@@ -4,13 +4,22 @@ import com.bbubbush.springplayground.entity.Member;
 import com.bbubbush.springplayground.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
   private final MemberRepository memberRepository;
   public Member findMemberById(Long id) {
     return memberRepository.findById(id)
       .orElse(Member.builder().build());
+  }
+
+  public Member save(String name) {
+    return memberRepository.save(
+      Member.builder()
+        .name(name)
+        .build());
   }
 }
